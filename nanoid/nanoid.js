cbed4c3a615b804c67a8d610e72bb0f3be7bce49
changhwan hwang <hch950627@naver.com>
2022-05-03 11:47:38 +0900

    nanoid custom

--- nanoid/nanoid.js
+++ nanoid/nanoid.js
@@ -1,3 +1,10 @@
-const { nanoid } = require("nanoid");
+const { nanoid, customAlphabet } = require("nanoid");
 
 console.log(nanoid()); // 일정 길이의 고유값 생성해줌
+
+const custom = customAlphabet(
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+  10
+); // 0~9,a~z,A~Z까지로 이루어진 10글자 nanoid
+
+console.log(custom());
